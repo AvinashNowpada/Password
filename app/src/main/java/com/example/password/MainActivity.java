4d3fd5ccda5191032a2e79c3811c7e2d35426c41
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2019-02-13 01:17:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/password/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.password;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        final EditText t = findViewById(R.id.editText);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!t.getText().toString().equals("1234")) {
+                    showAlertDialog(getApplicationContext(), "Authentication Failed", "Wrong pin", "Retry", "Exit");
+
+                } else {
+                    showAlertDialog(getApplicationContext(), "Authentication Sucess", "Welcome", "Ok", "Exit");
+                }
+            }
+        });
+    }
+
+    public void showAlertDialog(final Context context1, String title, String message, String posBtnMsg, String negBtnMsg) {
+        final AlertDialog.Builder builder ;
+        builder = new AlertDialog.Builder(context);
+
+        builder.setMessage(message).setTitle(message);
+
+        //Setting message manually and performing action on button click
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton(posBtnMsg, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle(title);
+        alert.show();
+    }
+
+
+}
